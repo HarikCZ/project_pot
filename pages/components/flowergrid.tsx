b6395e0fcc8659d23209793ec79995f4e125967e
HarikCZ <kuba.harcarik@gmail.com>
2022-12-20 15:37:20 +0100

    third commit
    zde přidávám material design (mui) a začínám s ním pracovat

--- pages/components/flowergrid.tsx
+++ pages/components/flowergrid.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
+import { Grid } from '@material-ui/core'
+
 import { Tile } from './flowertile';
 
 
 export const FlowerGrid = () => {
     return<>
-        
+        <Grid container spacing={3}>
+            
+            <Grid item xs={4}>
+                <Tile/>
+            </Grid>
+            <Grid item xs={4}>
+                <Tile/>
+            </Grid>
+            <Grid item xs={4}>
+                <Tile/>
+            </Grid>
+
+        </Grid>
     </>
 }
 
--- pages/components/flowertile.tsx
+++ pages/components/flowertile.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+
 
 export const Tile = () => {
     return <>
+        <Card>
+            <CardMedia
+            image="https://images.unsplash.com/photo-1530903677198-7c9f3577a63e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1852&q=80"
+            title="A beautiful flower that should be successfully displayed"
+        />
+        <CardContent>
+            <Typography variant="h5">
+                Název květiny
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+                Následný popis 
+            </Typography>
+      </CardContent>
+    </Card>
 
-            <Image src="https://thumbnail.imgbin.com/15/10/14/imgbin-flower-high-resolution-EUQkjDx9KY7GgkXEFzJtSbceh_t.jpg"></Image>
         
     
     </>       
